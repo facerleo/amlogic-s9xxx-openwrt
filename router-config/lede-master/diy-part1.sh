7469c8ca28c7e60eef01513a815c6e468876b4bc
facerleo <86334632+facerleo@users.noreply.github.com>
2022-09-20 22:18:25 -0700

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -19,4 +19,4 @@
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+# git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
